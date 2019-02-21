a1dc1be1c73df7193bd280e813bcb73b4b162cf3
Betsy Cowdery <emcowdery@gmail.com>
2019-02-21 15:23:32 -0500

    Split up all prior functions in to their own R scripts

---
+++ parameters/prior_calculations/prior_scripts/rdistn.R
@@ -0,0 +1,27 @@
+rdistn <- function(fit){
+
+  if(fit$distn == "beta"){
+    out <- rbeta(1000000, fit$parama, fit$paramb)
+  }else if(fit$distn == "unif"){
+    out <- runif(1000000, fit$parama, fit$paramb)
+  }else if(fit$distn == "exp"){
+    out <- rexp(1000000, fit$parama)
+  }else if(fit$distn == "gamma"){
+    out <- rgamma(1000000, fit$parama, fit$paramb)
+  }else if(fit$distn == "norm"){
+    out <- rnorm(1000000, fit$parama, fit$paramb)
+  }else if(fit$distn == "weibull"){
+    out <- rweibull(1000000, fit$parama, fit$paramb)
+  }else if(fit$distn == "lnorm"){
+    out <- rlnorm(1000000, fit$parama, fit$paramb)
+  }else if(fit$distn == "pois"){
+    out <- rpois(1000000, fit$parama)
+    # }else if(fit$distn == "t"){
+    #   out <- rt(1000000, fit$parama, fit$paramb, fit$paramc)
+    # }else if(fit$distn == "lt"){
+    #   out <- rt(1000000, fit$parama, fit$paramb, fit$paramc, log.p = TRUE)
+  }else{
+    out <- NA
+  }
+  return(out)
+}
