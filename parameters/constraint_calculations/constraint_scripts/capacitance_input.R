924c611ec8b9be15b3d0d676b0af1cbd94d7bf86
Betsy Cowdery <emcowdery@gmail.com>
2019-03-27 13:20:19 -0400

    Reorganizing and adding some new scripts

--- parameters/constraint_calculations/constraint_scripts/capacitance_input.R
+++ parameters/constraint_calculations/constraint_scripts/capacitance_input.R
@@ -24,23 +24,18 @@ cap <- cap %>% mutate( Cft_conv = case_when(
 
 ggplot(cap) + geom_density(aes(x = Cft_conv, col = Organ))
 
-###################
+################################################################################
 ## Wood Density
 
 wood_density_variable_id <- tbl(bety, "variables") %>% filter(name == "wood_density") %>% pull(id)
+
+# Note here we are choosing specifically the prior with id = 1000000281
 
 wood_density_fit <- tbl(bety, "priors") %>%
   filter(variable_id == wood_density_variable_id) %>%
   filter(id == 1000000281) %>% collect()
 wood_density_prior <- rdistn(wood_density_fit)
 wood_density_default <- get_ED_default("/fs/data3/ecowdery/ED.Hydro/parameters/pft3_defaults_history.xml", "rho")
-
-ggplot(cap) +
-  # geom_density(aes(x=WD, color = Organ)) +
-  geom_density(aes(x = WD)) +
-  geom_vline(aes(xintercept = default_wood_density)) +
-  ggtitle("Wood Density") +
-  geom_density(data = data.frame(wood_density_prior), aes(x = wood_density_prior))
 
 p <- prior_plot(prior = wood_density_prior,
                 plot_default = wood_density_default,
@@ -49,7 +44,7 @@ p <- prior_plot(prior = wood_density_prior,
 
 p + geom_density(data = cap, aes(x = WD, fill = "obs"), alpha = .3, color = NA)
 
-########################################################
+################################################################################
 ## Leaf Water Capacitance
 ## Leaf: gH2O g-1 dry weight MPa-1
 
@@ -57,67 +52,46 @@ leaf_water_cap_variable_id <- tbl(bety, "variables") %>%
   filter(name == "leaf_water_cap") %>% pull(id)
 leaf_water_cap_fit <- tbl(bety, "priors") %>%
   filter(variable_id == leaf_water_cap_variable_id) %>%
-  filter(distn == "lnorm") %>%
   collect()
 
 leaf_water_cap_prior <- rdistn(leaf_water_cap_fit) * (1/1000)
-
-leaf_water_cap_data <- cap %>% filter(Organ == "Leaf") %>% pull(Cft_conv)
-
-df_leaf <- rbind.data.frame(
-  data.frame( value = leaf_water_cap_prior, key = "leaf_water_cap_prior", stringsAsFactors = FALSE),
-  data.frame( value = leaf_water_cap_data, key = "leaf_water_cap_data", stringsAsFactors = FALSE)
-)
-
-ggplot(df_leaf) +
-  geom_density(aes(x = value, col = key))  +
-  xlim(0,1) +
-  geom_vline(aes(xintercept =  get_ED_default("/fs/data3/ecowdery/ED.Hydro/parameters/pft3_defaults_history.xml", "leaf_water_cap")))
-
-
-
+leaf_water_cap_data <- cap %>% filter(Organ == "Leaf")
 leaf_water_cap_default <- get_ED_default("/fs/data3/ecowdery/ED.Hydro/parameters/pft3_defaults_history.xml", "leaf_water_cap")
-
 
 prior_plot(prior = leaf_water_cap_prior,
            q = c(0,.995),
            plot_default = leaf_water_cap_default,
            title = sprintf("Leaf Water Cap"),
            type = "prior") +
-  geom_density(data = cap %>% filter(Organ == "Leaf"), aes(x = Cft_conv, fill = "obs"), alpha = .3, color = NA)
+  geom_density(data = leaf_water_cap_data, aes(x = Cft_conv, fill = "obs"), alpha = .3, color = NA)
 
-###############################################################################
+################################################################################
 ## Wood water capacitance
 ## Sapwood: kg m-3 MPa-1
 ## (~1/(1000*WD*MPa2M I guess)
 
-wood_water_cap_variable_id <- tbl(bety, "variables") %>% filter(name == "wood_water_cap") %>% pull(id)
+wood_water_cap_variable_id <- tbl(bety, "variables") %>%
+  filter(name == "wood_water_cap") %>% pull(id)
 wood_water_cap_fit <- tbl(bety, "priors") %>%
   filter(variable_id == wood_water_cap_variable_id) %>%
-  filter(distn == "lnorm") %>%
   collect()
+
 wood_water_cap_prior <- rdistn(wood_water_cap_fit) * (1/1000)
-
-wood_water_cap_data <- cap %>% filter(Organ == "Xylem") %>% pull(Cft_conv)
-
-df_wood <- rbind.data.frame(
-  data.frame( value = wood_water_cap_prior, key = "wood_water_cap_prior", stringsAsFactors = FALSE),
-  data.frame( value = wood_water_cap_data, key = "wood_water_cap_data", stringsAsFactors = FALSE)
-)
-
-ggplot(df_wood) +
-  geom_density(aes(x = value, col = key))  +
-  geom_vline(aes(xintercept =  get_ED_default("/fs/data3/ecowdery/ED.Hydro/parameters/pft3_defaults_history.xml", "wood_water_cap")))
-
+wood_water_cap_data <- cap %>% filter(Organ == "Xylem")
 leaf_water_cap_default <- get_ED_default("/fs/data3/ecowdery/ED.Hydro/parameters/pft3_defaults_history.xml", "leaf_water_cap")
-
 
 prior_plot(prior = wood_water_cap_prior,
            # q = c(0,1),
            plot_default = leaf_water_cap_default,
            title = sprintf("Wood Water Cap"),
            type = "prior") +
-  geom_density(data = cap %>% filter(Organ == "Xylem"), aes(x = Cft_conv, fill = "obs"), alpha = .3, color = NA)
+  geom_density(data = wood_water_cap_data, aes(x = Cft_conv, fill = "obs"), alpha = .3, color = NA)
+
+################################################################################
+## SLA
+
+get.trait.data()
+
 
 ################################################################################
 ##  Check that citations are in the database
---
+++ parameters/constraint_calculations/constraint_scripts/get.traits.R
@@ -0,0 +1,42 @@
+bety <- betyConnect("/fs/data3/ecowdery/pecan/web/config.php")
+
+variables <- c(
+  "SLA",
+  "wood_density",
+  "leaf_elastic_mod", "wood_elastic_mod",
+  "leaf_psi_osmotic", "wood_psi_osmotic",
+  "rwc_tlp_wood",
+  "leaf_water_sat", "wood_water_sat",
+  "leaf_water_cap", "wood_water_cap",
+  "leaf_psi_min", "wood_psi_min",
+  "leaf_psi_tlp",
+  "wood_Kmax",
+  "wood_psi50",
+  "wood_Kexp"
+)
+
+pftid <- 1000000131
+
+pft_info <- tbl(bety, "pfts") %>% filter(id == pftid) %>% collect()
+
+pft_prior_vars <- tbl(bety, "pfts_priors") %>% filter(pft_id == pftid) %>% inner_join(tbl(bety, "priors") %>% rename(prior_id = id),., by= "prior_id") %>% inner_join(tbl(bety, "variables") %>% rename(variable_id = id),., by = "variable_id" ) %>% collect() %>% pull(name)
+
+trait.names <- intersect(variables, pft_prior_vars)
+
+###############
+
+
+pft <- list()
+pft$name <- pft_info$name
+pft$outdir <- file.path("/fs/data3/ecowdery/ED.Hydro/parameters/constraints/", pft$name)
+
+pft = pft
+modeltype = tbl(bety, "modeltypes") %>% filter(id == pft_info$modeltype_id) %>% pull(name)
+dbfiles = "/fs/data1/pecan.data/dbfiles"
+dbcon = bety$con
+trait.names = trait.names
+forceupdate = FALSE
+
+get.trait.data.pft(pft, modeltype, dbfiles, dbcon, trait.names, forceupdate = TRUE)
+
+load(file.path(pft$outdir, "trait.data.Rdata"))
