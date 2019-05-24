f230fa02a883d7bcd40bfbd0851286e3226921c7
Betsy Cowdery <emcowdery@gmail.com>
2019-05-24 15:00:35 -0400

    Adding cheatsheets for reviewing citations and species

---
+++ parameters/constraint_calculations/constraint_scripts/review.species.R
@@ -0,0 +1,119 @@
+sp[which(sp$submit_name == "antirrhea trichantha"), "reviewed"] <- TRUE
+sp[which(sp$submit_name == "aspidosperma cruenta"), "reviewed"] <- TRUE
+sp[which(sp$submit_name == "bursera simarouba"), "reviewed"] <- TRUE
+sp[which(sp$submit_name == "chrysophllum cainito"), "reviewed"] <- TRUE
+sp[which(sp$submit_name == "hymenaea stignocarpa"), "reviewed"] <- TRUE
+sp[which(sp$submit_name == "hymenea courbaril"), "reviewed"] <- TRUE
+sp[which(sp$submit_name == "miconia pohliana"), "reviewed"] <- TRUE
+sp[which(sp$submit_name == "myrsine ferruginea"), "reviewed"] <- TRUE
+
+sp[which(sp$submit_name == "acinodendron pohlianum"),"reviewed"] <- TRUE
+sp[which(sp$submit_name == "antirrhea trichantha"), "reviewed"] <- TRUE
+sp[which(sp$submit_name == "aspidosperma cruenta"), "reviewed"] <- TRUE
+sp[which(sp$submit_name == "austromyrtus bidwillii"), "reviewed"] <- TRUE
+sp[which(sp$submit_name == "bauhinia variegate"), "reviewed"] <- TRUE
+sp[which(sp$submit_name == "eugenia ampullaria"), "reviewed"] <- TRUE
+sp[which(sp$submit_name == "eugenia bankensis"), "reviewed"] <- TRUE
+sp[which(sp$submit_name == "eugenia muelleri"), "reviewed"] <- TRUE
+sp[which(sp$submit_name == "miconia pohliana"), "reviewed"] <- TRUE
+sp[which(sp$submit_name == "millettia cubitti"), "reviewed"] <- TRUE
+sp[which(sp$submit_name == "myrsine ferruginea"), "reviewed"] <- TRUE
+sp[which(sp$submit_name == "piptadenia constricta"), "reviewed"] <- TRUE
+sp[which(sp$submit_name == "syzygium latilimbum"), "reviewed"] <- TRUE
+
+
+
+sp[which(sp$submit_name == "Acinodendron pohlianum"),"reviewed"] <- TRUE
+sp[which(sp$submit_name == "Antirrhea trichantha"), "reviewed"] <- TRUE
+sp[which(sp$submit_name == "Aspidosperma cruenta"), "reviewed"] <- TRUE
+sp[which(sp$submit_name == "Bauhinia variegate"), "reviewed"] <- TRUE
+sp[which(sp$submit_name == "Bursera simarouba"), "reviewed"] <- TRUE
+sp[which(sp$submit_name == "Chrysophllum cainito"), "reviewed"] <- TRUE
+sp[which(sp$submit_name == "Hymenaea stignocarpa"), "reviewed"] <- TRUE
+sp[which(sp$submit_name == "Hymenea courbaril"), "reviewed"] <- TRUE
+sp[which(sp$submit_name == "Miconia pohliana"), "reviewed"] <- TRUE
+sp[which(sp$submit_name == "Myrsine ferruginea"), "reviewed"] <- TRUE
+
+sp[which(sp$submit_name == "Austromyrtus bidwillii"), "reviewed"] <- TRUE
+sp[which(sp$submit_name == "Eugenia ampullaria"), "reviewed"] <- TRUE
+sp[which(sp$submit_name == "Eugenia bankensis"), "reviewed"] <- TRUE
+sp[which(sp$submit_name == "Eugenia muelleri"), "reviewed"] <- TRUE
+sp[which(sp$submit_name == "Millettia cubitti"), "reviewed"] <- TRUE
+sp[which(sp$submit_name == "Piptadenia constricta"), "reviewed"] <- TRUE
+sp[which(sp$submit_name == "Syzygium latilimbum"), "reviewed"] <- TRUE
+
+sp[which(sp$submit_name == "Michelia hypolampra"), "bety_name"] <- "Magnolia hypolampra"
+sp[which(sp$submit_name == "Michelia hypolampra"), "reviewed"]  <- TRUE
+
+sp[which(sp$submit_name == "Byrsonima crassa"), "reviewed"]  <- TRUE
+sp[which(sp$submit_name == "Symplocos mosenii"), "reviewed"]  <- TRUE
+sp[which(sp$submit_name == "Agathis borneensis"), "reviewed"]  <- TRUE
+sp[which(sp$submit_name == "Aglaia glabrata"), "reviewed"]  <- TRUE
+sp[which(sp$submit_name == "Canarium caudatum"), "reviewed"]  <- TRUE
+sp[grep("Castanopsis", sp$submit_name), "reviewed"]  <- TRUE
+sp[grep("Castanopsis", sp$submit_name), "bety_name"]  <- "Castanopsis indica"
+sp[which(sp$submit_name == "Cordia collococa"), "bety_name"] <- "Cordia nodosa"
+sp[which(sp$submit_name == "Cordia collococa"), "reviewed"]  <- TRUE
+sp[which(sp$submit_name == "Cotylelobium burckii"), "reviewed"]  <- TRUE
+sp[which(sp$submit_name == "Diospyros brachiata"), "bety_name"] <- "Diospyros dictyoneura"
+sp[which(sp$submit_name == "Diospyros brachiata"), "reviewed"]  <- TRUE
+sp[which(sp$submit_name == "Diospyros mindanaensis"), "reviewed"]  <- TRUE
+sp[which(sp$submit_name == "Dipterocarpus globosus"), "reviewed"]  <- TRUE
+sp[which(sp$submit_name == "Dryobalanops aromatica"), "reviewed"]  <- TRUE
+sp[which(sp$submit_name == "Dryobalanops sumatrensis"), "reviewed"]  <- TRUE
+sp[which(sp$submit_name == "Dussia munda"), "reviewed"]  <- TRUE
+sp[which(sp$submit_name == "Galbulimima baccata"), "bety_name"] <- "Galbulimima belgraveana"
+sp[which(sp$submit_name == "Galbulimima baccata"), "reviewed"]  <- TRUE
+sp[which(sp$submit_name == "Heritiera sumatrana"), "reviewed"]  <- TRUE
+sp[which(sp$submit_name == "Isonandra lanceolata"), "reviewed"]  <- TRUE
+sp[which(sp$submit_name == "Lophopetalum subobovatum"), "reviewed"]  <- TRUE
+sp[which(sp$submit_name == "Mallotus wrayi"), "reviewed"]  <- TRUE
+sp[which(sp$submit_name == "Payena endertii"), "reviewed"]  <- TRUE
+sp[which(sp$submit_name == "Pentace adenophora"), "reviewed"]  <- TRUE
+sp[which(sp$submit_name == "Pistacia weinmannifolia"), "bety_name"] <- "Pistacia weinmanniifolia"
+sp[which(sp$submit_name == "Pistacia weinmannifolia"), "reviewed"]  <- TRUE
+sp[which(sp$submit_name == "Santiria mollis"), "reviewed"]  <- TRUE
+sp[which(sp$submit_name == "Shorea ovalis"), "reviewed"]  <- TRUE
+sp[which(sp$submit_name == "Sindora leiocarpa"), "reviewed"]  <- TRUE
+sp[which(sp$submit_name == "Stemonurus umbellatus"), "reviewed"]  <- TRUE
+sp[which(sp$submit_name == "Tachigalia versicolor"), "bety_name"] <- "Tachigali versicolor"
+sp[which(sp$submit_name == "Tachigalia versicolor"), "reviewed"]  <- TRUE
+sp[which(sp$submit_name == "Trimenia neocaledonica"), "reviewed"]  <- TRUE
+sp[which(sp$submit_name == "Zygogynum baillonii"), "reviewed"]  <- TRUE
+sp[which(sp$submit_name == "Zygogynum bicolor"), "reviewed"]  <- TRUE
+sp[which(sp$submit_name == "Zygogynum pomiferum"), "reviewed"]  <- TRUE
+sp[which(sp$submit_name == "Zygogynum queenslandianum"), "reviewed"]  <- TRUE
+sp[which(sp$submit_name == "Zygogynum semecarpoides"), "reviewed"]  <- TRUE
+
+submit_name case
+1       antirrhea trichantha    7
+2       aspidosperma cruenta    7
+3         bauhinia variegate    7
+4          bursera simarouba    7
+5       chrysophllum cainito    7
+6          hymenea courbaril    7
+7           micranda sprucei    7
+8          rhedera trinervis    7
+9      sweitenia macrophylla    7
+10          heteropterys sp.    8
+11              shorea guiso    8
+12         shorea parvifolia    8
+13      barringtonia pendula   11
+14 catostemma sancarlosianum   11
+15          cordia americana   11
+16    crossopteryx febrifuga   11
+17         irvingia malayana   11
+18      maytenus obtusifolia   11
+19           pachira sordida   11
+20      palaquium sumatranum   11
+21     rhododendron arboreum   11
+22         shorea macroptera   11
+23     tachigalia versicolor   11
+24 ternstroemia brasiliensis   11
+25     thouinidium decandrum   11
+26            vatica odorata   11
+27      mallotus penangensis   12
+28       michelia hypolampra   12
+29    millettia atropurpurea   12
+30     parashorea densiflora   12
+31       quercus semiserrata   12
